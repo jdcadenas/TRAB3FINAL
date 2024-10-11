81136b7b7498016121ef04c037eb8f4a5dbd14e6
José Cadenas <jdcadenas@gmail.com>
2024-10-11 11:07:30 -0400

    se ordena el código y los pacientes vip

--- centroMedico.c
+++ centroMedico.c
@@ -8,24 +8,22 @@
 #include <signal.h>
 #include <time.h>
 
-
-
-#define NUM_MEDICOS 4 // atienden 4 medicos
-#define NUM_CAJEROS 2 // 2 cajeros
-#define CAPACIDAD_CENTRO 28 // 28 pacientes dentro del consultorio
-#define NUM_FIAS NUM_MEDICOS  // una fila para cada medico
-#define FILA_VIP 4 // indica fila vip
+#define NUM_MEDICOS 4        // atienden 4 medicos
+#define NUM_CAJEROS 2        // 2 cajeros
+#define CAPACIDAD_CENTRO 28  // 28 pacientes dentro del consultorio
+#define NUM_FIAS NUM_MEDICOS // una fila para cada medico
+#define FILA_VIP 4           // indica fila vip
 
 // semaforos
 sem_t capacidad_consultorio;
 sem_t medicos_sem[NUM_MEDICOS];
 sem_t cajeros[NUM_CAJEROS];
-sem_t abonar_consulta ;
+sem_t abonar_consulta;
 sem_t consulta_terminada;
 sem_t mutex;
 sem_t fila_vip;
 
-bool terminar; 
+bool terminar;
 int pacientes_por_medico[NUM_MEDICOS] = {0};
 int pacientes_vip_por_medico[NUM_MEDICOS] = {0};
 bool hay_paciente_vp = false;
@@ -33,81 +31,81 @@ bool hay_paciente_vp = false;
 void cajero(void *arg);
 void paciente(void *arg);*/
 
-
-void handle_signal(int sig) {
+void handle_signal(int sig)
+{
     int total_clientes = 0;
-    if (sig == SIGINT || sig == SIGTERM){
+    if (sig == SIGINT || sig == SIGTERM)
+    {
         terminar = true;
         printf("Resultado de consultorio\n");
         printf("Handle signal %d\n", sig);
         for (int i = 0; i < NUM_MEDICOS; i++)
         {
-            printf("El meddico  %d atendio %d pacientes\n", i+1, pacientes_por_medico[i]);
+            printf("El meddico  %d atendio %d pacientes\n", i + 1, pacientes_por_medico[i]);
             printf("y %d Pacientes VIP\n", pacientes_vip_por_medico[i]);
             total_clientes += pacientes_por_medico[i] + pacientes_vip_por_medico[i];
-
         }
         printf("________________________________________________\n");
-        printf("Total de clientes atendidoses de : %d \n" ,total_clientes);
-
+        printf("Total de clientes atendidoses de : %d \n", total_clientes);
     }
     exit(0);
 }
 
-
-    void *medico(void *arg)
+void *medico(void *arg)
+{
+    int id_medico = *(int *)arg;
+    while (1)
     {
-        int id_medico = *(int *)arg;
-        while(1)
+        sem_wait(&mutex);
+        // Primero verificar si hay pacientes VIP
+        if (hay_paciente_vp || (pacientes_por_medico[id_medico] % 3 == 0))
         {
-             if(hay_paciente_vp)
-            {
-                sem_wait(&fila_vip);
-                printf("Atendiendo vip\n");
-            }
-
+            sem_wait(&fila_vip);
+            printf("Médico %d atendiendo a un paciente VIP\n", id_medico);
+            pacientes_vip_por_medico[id_medico]++;
+        }else{
+            // Si no hay VIPs, esperar por un paciente regular
             sem_wait(&medicos_sem[id_medico]);
-            printf("Medico %d atiende paciente",id_medico);
-            sem_post(&abonar_consulta);
+            printf("Médico %d atendiendo a un paciente regular\n", id_medico);
+            pacientes_por_medico[id_medico]++;
         }
-    }
+        sem_post(&mutex);
 
-    void *cajero(void *arg)
+        sem_post(&abonar_consulta);
+    }
+}
+
+void *cajero(void *arg)
+{
+    while (1)
     {
-        while(1)
-        {
-            sem_wait(&abonar_consulta);
-            printf("Cliente abonando la consulta\n");
-            sem_post(&capacidad_consultorio);
-        }
-
-
+        sem_wait(&abonar_consulta);
+        printf("Cliente abonando la consulta\n");
+        sem_post(&capacidad_consultorio);
     }
-     void *paciente(void *arg)
+}
+void *paciente(void *arg)
 
-    { 
-        int fila; 
-        while(1)
+{
+    int fila;
+    while (1)
+    {
+        sem_wait(&capacidad_consultorio);
+        printf("Cliente esperando ser atendido\n");
+        sem_wait(mutex);
+        fila = rand() % 4;
+        if (fila == FILA_VIP)
         {
-            sem_wait(&capacidad_consultorio);
-            printf("Cliente esperando ser atendido\n");
-            sem_wait(mutex);
-            fila = rand() % 4;
-            if(fila == FILA_VIP)
-            {
-                hay_paciente_vp = true;
-                sem_post(&fila_vip);
-            }
-            else
-            {
-                sem_post(&medicos_sem[fila]);
-            }
-            sem_post(mutex);
+            hay_paciente_vp = true;
+            sem_post(&fila_vip);
         }
-        
-
+        else
+        {
+            sem_post(&medicos_sem[fila]);
+        }
+        sem_post(mutex);
     }
-
+}
 
 int main()
 {
@@ -128,49 +126,46 @@ int main()
     sem_init(&capacidad_consultorio, 0, 28);
     for (int i = 0; i < NUM_MEDICOS; i++)
     {
-        sem_init(&medicos_sem[i],0,0);
-        
+        sem_init(&medicos_sem[i], 0, 0);
     }
 
-     for (int i = 0; i < NUM_CAJEROS; i++)
+    for (int i = 0; i < NUM_CAJEROS; i++)
     {
-        sem_init(&cajeros[i],0,0);
-        
+        sem_init(&cajeros[i], 0, 0);
     }
-    sem_init(&abonar_consulta,0,0);
-    sem_init(&consulta_terminada,0,0);
-    sem_init(&mutex,0,1);
-    sem_init(& fila_vip,0,0);
+    sem_init(&abonar_consulta, 0, 0);
+    sem_init(&consulta_terminada, 0, 0);
+    sem_init(&mutex, 0, 1);
+    sem_init(&fila_vip, 0, 0);
 
     /*Creacion Hilos*/
 
-    while(! terminar)
+    while (!terminar)
     {
         for (int i = 0; i < NUM_MEDICOS; i++)
         {
-            pthread_create(&h_medicos[i], NULL,medico, (void*)&i);
-            printf("Creando hilo de medico %d \n",i);
+            pthread_create(&h_medicos[i], NULL, medico, (void *)&i);
+            printf("Creando hilo de medico %d \n", i);
         }
-          for (int i = 0; i < NUM_CAJEROS; i++)
+        for (int i = 0; i < NUM_CAJEROS; i++)
         {
-            pthread_create(&h_cajeros[i], NULL,cajero,NULL);
-            printf("Creando hilo de cajero %d \n",i);
+            pthread_create(&h_cajeros[i], NULL, cajero, NULL);
+            printf("Creando hilo de cajero %d \n", i);
         }
-          pthread_create(&h_pacientes,NULL,paciente,NULL);
-       
+        pthread_create(&h_pacientes, NULL, paciente, NULL);
     }
     sem_destroy(&capacidad_consultorio);
     for (int i = 0; i < NUM_MEDICOS; i++)
     {
         sem_destroy(medicos_sem[NUM_MEDICOS]);
-    
-     for (int i = 0; i < NUM_CAJEROS; i++)
-    {
-        sem_destroy(cajeros[NUM_CAJEROS]);
+
+        for (int i = 0; i < NUM_CAJEROS; i++)
+        {
+            sem_destroy(cajeros[NUM_CAJEROS]);
+        }
+        sem_destroy(&abonar_consulta);
+        sem_destroy(&consulta_terminada);
+        sem_destroy(&mutex);
+        return 0;
     }
-    sem_destroy(&abonar_consulta);
-    sem_destroy(&consulta_terminada);
-    sem_destroy(&mutex);
-    return 0;
-}
 }
